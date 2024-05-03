5d94a84b8811782b68e00c9c33ceae807246a121
Delirium <Amal1@Delirium.local>
2024-05-03 20:55:49 +0200

    bee

--- pages/introduction.py
+++ pages/introduction.py
@@ -4,4 +4,4 @@ st.set_page_config(page_title="EasyPressure", page_icon="🫀", layout="wide")
 menu(authenticated=True)
 
 st.title("This page is available to all users")
-st.markdown(f"You are currently logged with the role of .")
+st.markdown(f"hallo.")
