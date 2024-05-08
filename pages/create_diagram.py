c583cfadd583860bbbd6ace3ce55d371e9adcd8f
Delirium <Amal1@Delirium.local>
2024-05-08 20:36:38 +0200

    save

--- pages/create_diagram.py
+++ pages/create_diagram.py
@@ -109,7 +109,18 @@ if user_data is not None:
 
 
 
-        
+        if st.button('Diagramm speichern', help='Lokale Schpeicherung Ihres Diagramms'):
+            fig.write_image(chart_path,format='png', width=1600, height=1200)
+            with open(chart_path, "rb") as file:
+                btn = st.download_button(
+                label="Diagramm herunterladen",
+                data=file,
+                file_name=f"{current_date}.png",
+                mime="image/png")
+            st.write("Saving chart to:", chart_path)
+            
+            st.success('Diagramm erfolgreich gespeichert!')
+            time.sleep(5)
 
         if st.button('Diagramm speichern', help='Lokale Schpeicherung Ihres Diagramms'):
             try:
@@ -127,8 +138,8 @@ if user_data is not None:
                 st.write("Saving chart to:", chart_path)
                 st.success('Diagramm erfolgreich gespeichert!')
                 
-            except Exception:
-                st.error(f"Ein Fehler ist aufgetreten.")
+            except Exception as e:
+                st.error(f"Ein Fehler ist aufgetreten:  {str(e)}")
         
         token = get_auth_token()
         cookie_options ={'max_age': 86400 }
