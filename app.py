6f19146e3574ecd2cf71db441a82ec4c17b0096d
Delirium <Amal1@Delirium.local>
2024-05-18 20:26:40 +0200

    theme in config new try

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 from utility.auth_utilities import get_user_data
 
-st.set_page_config(page_title="EasyPressure", page_icon="🫀",)
+st.set_page_config(page_icon="🫀",)
 
 dotenv_path=join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
