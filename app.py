1dc2a809350e6dfc56d5571c708828ecb6d6e2f3
Delirium <Amal1@Delirium.local>
2024-05-19 21:31:30 +0200

    new 19.05 21_31

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def get_image_base64(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
 
-controller = None
+
 def main():
     image_path = 'docs/anmelden.jpeg' 
     encoded_image = get_image_base64(image_path)  
