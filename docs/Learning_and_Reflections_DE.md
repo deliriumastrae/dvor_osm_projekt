6f7da742651a814a1ebd7c9fd15f22f22ba8a738
Delirium <Amal1@Delirium.local>
2024-06-02 21:50:39 +0200

    final

--- docs/Learning_and_Reflections_DE.md
+++ docs/Learning_and_Reflections_DE.md
@@ -18,6 +18,17 @@ Das EasyPressure-Anwendungsprojekt ist darauf ausgelegt, Blutdruck- und Pulswert
 - Wir stießen auf Herausforderungen beim Implementieren der Datenbankanbindung. Durch Online-Recherchen, Austausch mit erfahrenen Entwicklern und selbstvertandlich ChatGPT fanden wir effektive Lösungsansätze.
 - Sicherheitsbedenken wurden durch das Erlernen und Implementieren von Authentifizierungsprotokollen wie JWT und bcrypt adressiert.
 
+### Probleme und Lösungen
+1. **Verlust des Authentifizierungstokens bei Seitenaktualisierung**: In der aktuellen Version des Projekts geht das Authentifizierungstoken bei der Verwendung von `st.sessionstate` verloren. Auf Empfehlung von Dominik haben wir das Projekt auf diese Version zurückgesetzt. Zuvor haben wir das Token mit Hilfe von `CookieController` in Cookies gespeichert. Dabei wurde das Token anscheinend auf dem Streamlit-Server gespeichert, so dass der Benutzer als erfolgreich authentifiziert galt, obwohl er weder Passwort noch Login eingegeben hatte.
+2. **Verzögerte Anwendung des benutzerdefinierten Themes**: Manchmal wird mein benutzerdefiniertes Theme nicht sofort angewendet, sondern erst nach der Authentifizierung und einer Seitenaktualisierung. Dieses Problem scheint mit meinem Browser (Chrome) zusammenzuhängen.
+3. **Fehlerhafte Speicherung von Diagrammen**: Die Diagramme wurden ohne zusätzliche Verarbeitung nicht korrekt gespeichert. Um dieses Problem zu lösen, mussten wir `kaleido` verwenden.
+4. **Anpassung der Schaltfläche, die die Sidebar öffnet und schließt**: Die Anpassung der Schaltfläche mit HTML verursachte ebenfalls Schwierigkeiten. Lokal funktioniert alles wie erwartet, aber in der Cloud kann es zu Instabilitäten kommen.
+
+### Zusätzliche Hinweise der Entwickler
+1. **Anpassung des Interfaces für ältere Menschen**: Wir haben das Interface speziell für ältere Benutzer angepasst, indem wir größeren und fetteren Text und umgestaltete Schaltflächen verwendet haben. Diese Änderungen wurden durch HTML implementiert, um die Lesbarkeit und Bedienbarkeit zu verbessern.
+2. **Datenversandfunktion**: Die Funktion zum Versenden von Daten wurde so implementiert, dass sie sicher und benutzerfreundlich ist. Alternativ hätten wir Zugriff auf das E-Mail-Konto des Benutzers benötigt, um automatisch Diagramme anhängen zu können, was den Zugang zu persönlichen Passwörtern des Benutzers erfordert hätte.
+3. **Anzeigenormwerte**: Die App zeigt keine Normalwerte für Messdaten an, um keine diagnostische Verantwortung zu übernehmen. Die Interpretation der Daten sollte von einem Arzt vorgenommen werden, um die medizinische Genauigkeit und Verantwortlichkeit zu gewährleisten.
+
 ## Zukünftige Verbesserungen und Lektionen
 - Für zukünftige Versionen könnte man die Interaktionsmöglichkeiten zu erweitern und mehr personalisierte Feedbackoptionen für Benutzer zu integrieren.
 - Wir haben die Bedeutung von gründlichem Testen und Feedback-Einholung gelernt, um ein qualitativ hochwertiges Produkt sicherzustellen.
--- pages/introduction.py
+++ pages/introduction.py
@@ -4,20 +4,22 @@ st.set_page_config(page_title="EasyPressure", page_icon="🫀",layout="wide")
 menu(authenticated=True)
 
 def main():
-    st.title('Willkommen zur Einführung')
-    st.write("""
-    Diese Anwendung ermöglicht es Benutzern, ihre Blutdruckwerte und Pulswerte einzugeben und zu verfolgen.
-    Sie können die Werte eingeben und die Trends über die Zeit anzeigen lassen.
-             
-Funktionen der Anwendung:
+    st.title("Willkommen zur Einführung in EasyPressure")
 
-**Eingabe von Blutdruck- und Pulswerten:** Benutzer können ihre Blutdruck- und Pulswerte in die Anwendung eingeben und verwalten.
-             
-**Verfolgung von Blutdruck- und Pulswerten:** Die Anwendung ermöglicht es Benutzern, ihre eingegebenen Blutdruck- und Pulswerte im Laufe der Zeit zu verfolgen. Sie können Trends erkennen und mögliche Veränderungen im Gesundheitszustand überwachen.
-             
-**Datenvisualisierung:** Die Anwendung bietet Funktionen zur Visualisierung der eingegebenen Daten, z.B. durch Diagramme oder Grafiken. Dadurch können Benutzer ihre Daten besser verstehen und interpretieren.
-             
-**Benutzerfreundliche Oberfläche:** Die Anwendung bietet eine benutzerfreundliche Oberfläche, die es Benutzern leicht macht, ihre Daten einzugeben, zu verfolgen und zu analysieren. Streamlit ermöglicht es, interaktive Elemente wie Slider, Dropdown-Menüs und Schaltflächen einzubinden, um die Benutzererfahrung zu verbessern.
+    st.write("""
+    Diese Anwendung wurde speziell entwickelt, um Benutzern das Eingeben, Verwalten und Überwachen ihrer Blutdruck- und Pulswerte zu erleichtern. Mit EasyPressure können Sie nicht nur Ihre Gesundheitsdaten effektiv verfolgen, sondern diese auch direkt an Ihren Arzt weiterleiten, was eine nahtlose Kommunikation und bessere medizinische Betreuung ermöglicht.
     """)
+
+    st.header("Funktionen der Anwendung:")
+
+    st.markdown("""
+    - **Eingabe von Blutdruck- und Pulswerten**: Geben Sie Ihre Werte sicher und einfach ein und speichern Sie diese in Ihrem Benutzerprofil. Diese Daten helfen Ihnen, ein persönliches Gesundheitsregister zu führen.
+    - **Verfolgung von Blutdruck- und Pulswerten**: Überwachen Sie Ihre Werte über die Zeit, um Trends und signifikante Änderungen in Ihrem Gesundheitszustand zu erkennen. Diese Funktion ist besonders wertvoll, um präventive Maßnahmen zu ergreifen oder die Wirksamkeit von medizinischen Behandlungen zu beurteilen.
+    - **Datenvisualisierung**: Nutzen Sie fortschrittliche Diagramme und Grafiken, um Ihre Blutdruck- und Pulswerte anschaulich darzustellen. Unsere visuellen Tools sind darauf ausgerichtet, Ihnen ein klares Bild Ihrer Gesundheitsentwicklung zu vermitteln.
+    - **Benutzerfreundliche Oberfläche**: Die Anwendung zeichnet sich durch eine intuitive und leicht zugängliche Benutzeroberfläche aus. Dank der Verwendung von Streamlit integrieren wir interaktive Elemente wie Slider, Dropdown-Menüs und Schaltflächen, die die Benutzererfahrung signifikant verbessern.
+    - **Sichere Datenübermittlung an Ärzte**: Mit nur wenigen Klicks können Sie Ihre Gesundheitsdaten sicher an Ihren Arzt übermitteln. Diese Funktion fördert eine effektive Kommunikation zwischen Ihnen und Ihrem medizinischen Betreuer und unterstützt eine datenbasierte Diagnostik und Behandlung.
+    """)
+
+    st.info("Durch die Nutzung von EasyPressure können Sie aktiv an der Überwachung und Verbesserung Ihrer Gesundheit mitwirken, während Sie gleichzeitig von einer Technologie profitieren, die Sicherheit, Benutzerfreundlichkeit und Effizienz in den Vordergrund stellt.")
 if __name__ == '__main__':
     main()
